38876939c54395f3719d7f612d0c79bb592be4e3
dimas lópez <me@dimaslz.io>
2021-03-15 14:04:57 +0100

    feat: copy command to clipboard

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  onClickCommandExample($event: MouseEvent): void {
+    const target: EventTarget | null = $event.target;
+
+    if (target instanceof Element) {
+      const value = target?.textContent as string;
+      this.copyToClipboard(value.replace('$ ', ''));
+    }
+  }
+
+  copyToClipboard(str: string): void {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+}
