36532312b67b67cc5fcaef4078b7bed4114f242c
dimaslz <me@dimaslz.io>
2021-08-11 01:27:10 +0200

    test(app): fix test mocking components

--- projects/playground/src/app/app.component.spec.ts
+++ projects/playground/src/app/app.component.spec.ts
@@ -1,17 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 @Component({ selector: 'icons', template: '' })
 class IconsComponent {}
 
+@Component({ selector: 'app-darkmode-toggle', template: '' })
+class DarkModeComponent {}
+
+@Component({
+  selector: 'app-icon',
+  template: ''
+})
+class IconComponent {
+  @Input() size = 24;
+}
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, IconsComponent],
+      declarations: [AppComponent, IconsComponent, DarkModeComponent, IconComponent],
     }).compileComponents();
   });
 
