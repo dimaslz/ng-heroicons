96c1ec279a8ba05936103b9fce5c1842cf2c2dc5
dimaslz <me@dimaslz.io>
2021-08-15 16:21:05 +0200

    chore(ci): improve release script

--- .release-it.js
+++ .release-it.js
@@ -1,12 +1,30 @@
 module.exports = {
   "hooks": {
-    // "before:init": ["npm run lint", "npm test"],
+    "before:init": ["yarn test"],
     // "after:my-plugin:bump": "./bin/my-script.sh",
     "after:bump": "NODE_ENV=production yarn build ng-heroicons",
     "after:git:release": "echo After git push, before github release",
     "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
   },
   "git": {
-    "commitMessage": "chore: release v${version}"
+    "commitMessage": "chore: release v${version}",
+    "release": true
+  },
+  "npm": {
+    "publish": false,
+    "ignoreVersion": true,
+    "publishPath": "dist/ng-heroicons",
+    "publishConfig": {
+      "access": "public"
+    }
+  },
+  "plugins": {
+    "@release-it/bumper": {
+      "out": ["projects/ng-heroicons/package.json", "dist/ng-heroicons/package.json"]
+    },
+    "@release-it/conventional-changelog": {
+      "preset": "conventionalcommits",
+      "infile": "CHANGELOG.md"
+    }
   }
 }
