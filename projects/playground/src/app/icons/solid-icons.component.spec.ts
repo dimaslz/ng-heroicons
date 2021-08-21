478310eafee3c8a6202a4d357769ea21b7acd316
dimaslz <me@dimaslz.io>
2021-08-21 18:30:29 +0200

    test(playground): add test to validate all the components are present

---
+++ projects/playground/src/app/icons/solid-icons.component.spec.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { render } from '@testing-library/angular';
+import kebabcase from 'lodash.kebabcase';
+
+import * as SOLID_ICONS from '../../../../ng-heroicons/src/lib/heroicons/solid';
+
+import {SolidIconsComponent} from './solid-icons.component';
+
+const ICONS_ARRAY = Object.entries(SOLID_ICONS);
+
+describe('Solid icons', () => {
+	let fixture: any;
+  let container: any;
+  beforeEach(async () => {
+    jest.useFakeTimers();
+
+    const component = await render(SolidIconsComponent, {
+      imports: [
+        CommonModule,
+			],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+
+    fixture = component.fixture;
+    container = component.container;
+	});
+
+	it('should create the app', () => {
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
+
+	let componentsCounter = 0;
+	it.each(ICONS_ARRAY)(
+		'should be component: %s', (name) => {
+		const componentTag = kebabcase(name).replace('-component', '');
+		expect(container.querySelector(componentTag)).toBeTruthy();
+
+		componentsCounter++;
+	});
+
+	it(`should be ${ICONS_ARRAY.length} components`, async () => {
+		expect(ICONS_ARRAY.length).toBe(componentsCounter);
+	});
+});
