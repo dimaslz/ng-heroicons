e55d623bdfcfa7978bd22caec87ba1f1e3eccafc
dimaslz <me@dimaslz.io>
2022-01-03 23:21:46 +0000

    fix(tailwind): fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,19 @@ const enablePurge = process.env.NODE_ENV === "production" || false;
 
 module.exports = {
   important: true,
-  purge: {
-    enabled: enablePurge,
-    content: [
-      "./projects/**/*.html",
-      "./projects/**/*.scss",
-      "./projects/**/*.ts",
-    ],
-    safelist: [
-      "bg-white",
-      "text-white",
-      /^bg-(gray|red|yellow|green|blue|purple|indigo)-400$/,
-      /^text-(gray|red|yellow|green|blue|purple|indigo)-(900|400)$/,
-    ],
-  },
+  content: [
+    "./projects/**/*.{html,scss,ts}",
+  ],
+  safelist: [
+    "bg-white",
+    "text-white",
+    {
+      pattern: /^bg-(gray|red|yellow|green|blue|purple|indigo)-400$/,
+    },
+    {
+      pattern: /^text-(gray|red|yellow|green|blue|purple|indigo)-(900|400)$/,
+    },
+  ],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
