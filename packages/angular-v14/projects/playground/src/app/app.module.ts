90be1a7df817a75025939ec769c0b2aec0d3c6e8
dimaslz <me@dimaslz.io>
2022-10-30 19:23:10 +0100

    chore(packages): add angular v14

---
+++ packages/angular-v14/projects/playground/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { NgHeroiconsModule } from 'lib';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    NgHeroiconsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
