6cf446dd0dde8ca5dd1ad506b898724e096b1a5a
Dimas López <me@dimaslz.io>
2024-02-11 06:25:37 +0100

    chore(landing): move to SSR (#413)
    * chore(landing): move to SSR
    
    * chore(landing/ci): add CI

--- landing/setupJest.ts
+++ landing/setupJest.ts
@@ -1,2 +1,16 @@
 import 'jest-preset-angular/setup-jest';
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
